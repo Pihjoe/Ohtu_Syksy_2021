7ab65edc9744b0741eaa51a793a8649929ff9d07
Joel <joel.pihlaja@helsinki.fi>
2021-11-29 22:29:56 +0200

    Tehtävä 4.5.7

--- koodi/viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ koodi/viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -39,3 +39,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(len(self.kori.kori), 2)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_2_kertaa_tuotteen_hinta(self):
+        maito = Tuote("Maito", 3)
+        leipa = Tuote("Leipä", 8)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.hinta(), 6)
